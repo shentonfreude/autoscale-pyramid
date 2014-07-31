fca855696cc21885b30d64838e161be81f9d59ac
Chris Shenton <chris@koansys.com>
2014-07-31 14:00:45 -0400

    factorial, squareroot and machine metadata

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import platform
+
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+
+def _metadata():
+    return {'metadata': {'node': platform.node()}}
+
+def metadata(request):
+    return _metadata()
 
 def _factorial(number):
     # TODO: Should sanity check number < 1 and raise Exception
@@ -15,7 +23,10 @@ def _factorial(number):
 def factorial(request):
     # TODO: Sanity check number is int
     number = int(request.matchdict['number'])
-    return Response('Factorial of %d is %d' % (number, _factorial(number)))
+    #return Response('Factorial of %d is %d' % (number, _factorial(number)))
+    md = _metadata()
+    md.update({'number': number, 'factorial': _factorial(number)})
+    return md
 
 def _squareroot(number):
     # Newton's method
@@ -34,20 +45,33 @@ def _squareroot(number):
 def squareroot(request):
     number = int(request.matchdict['number'])
     sr, steps = _squareroot(number)
-    return Response('Squareroot of %d is %f in %d steps' %
-                    (number, sr, steps))
+    md = _metadata()
+    md.update({'number': number, 'squareroot': sr, 'steps': steps})
+    return md
+
+def home(request):
+    return {'methods': ['/', '/metadata/{n}', '/squareroot/{n}', '/hello/{name}']}
 
 def hello_world(request):
-    return Response('Hello %(name)s!' % request.matchdict)
+    md = _metadata()
+    md.update({'msg': 'Hello %(name)s!' % request.matchdict})
+    return md
 
 if __name__ == '__main__':
     config = Configurator()
-    config.add_route('factorial', '/factorial/{number}')
-    config.add_view(factorial, route_name='factorial')
+
+    config.add_route('home',       '/')
+    config.add_route('metadata',   '/metadata')
+    config.add_route('factorial',  '/factorial/{number}')
     config.add_route('squareroot', '/squareroot/{number}')
-    config.add_view(squareroot, route_name='squareroot')
-    config.add_route('hello', '/hello/{name}')
+    config.add_route('hello',      '/hello/{name}')
+
+    config.add_view(home,        route_name='home',       renderer='json')
+    config.add_view(metadata,    route_name='metadata',   renderer='json')
+    config.add_view(factorial,   route_name='factorial',  renderer='json')
+    config.add_view(squareroot,  route_name='squareroot', renderer='json')
     config.add_view(hello_world, route_name='hello')
+
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
     server.serve_forever()
