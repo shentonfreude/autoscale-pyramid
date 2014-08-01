f8adf15e585da3891a242652d1ec32a5251aa10b
Chris Shenton <chris@koansys.com>
2014-08-01 12:55:15 -0400

    Clearer name, emit some messages for AWS logs

--- aws-ec2-userdata.sh
+++ aws-ec2-userdata.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo `date` RUNNING USER DATA
+
 # Ubuntu 14.04 has python-2.7
 apt-get update -y
 apt-get install -y git python-virtualenv
@@ -6,13 +8,16 @@ apt-get install -y git python-virtualenv
 # become mortal, change to ~ubuntu
 su - ubuntu
 
-# Should do a tag
+# TODO: use a tag
 # https avoids git: ssh asking to trust the key, hanging AWS
 git clone https://github.com/shentonfreude/autoscale-pyramid
 cd autoscale-pyramid ~
 
+# Build and run with scripts in the code repo for easy changes
+# since AWS doesn't let us edit the User Data.
 ./build.sh
 # Should run in background or AWS init might never complete?
 ./run.sh
 
+echo `date` FINISHED USER DATA AND BUILDING / RUNNING
 
